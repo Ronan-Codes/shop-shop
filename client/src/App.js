16c24d6715cc98f42b1b45b0a1f6945844478bb1
Ronan-Codes <galv.ronan@gmail.com>
2021-10-25 12:48:56 -0400

    Added GlobalState and incorporated it into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,11 @@ import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import OrderHistory from './pages/OrderHistory';
 
+import { StoreProvider } from './utils/GlobalState';
+// insert <StoreProvider> after <Router>
+// We can now use our useStoreContext() function to grab the state from the <StoreProvider> 
+  // component and use the returning dispatch method to update it
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
@@ -40,15 +45,17 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext } from "react";
+// instantiate a new Context object. Create the container to hold our global state data and 
+    // functionality so we can provide it throughout our app!
+
+import { useProductReducer } from "./reducers";
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props}) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />
+}
+
+const useStoreContext = () => {
+    return useContext(StoreContext)
+}
+// Created our own custom React Hook. This means that any component that has access to our 
+    // StoreProvider component can use any data in our global state container or update it 
+    // using the dispatch function. 
+
+export { StoreProvider, useStoreContext };
